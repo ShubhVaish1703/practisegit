b03d5ba8ea3159d8ade44d21059884f3a35b70a3
ShubhVaish1703 <114241988+ShubhVaish1703@users.noreply.github.com>
2023-08-22 14:36:33 +0530

    push

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 def addtion(a,b+e):
     return a+b+e 
-=======
-def addtion(a,b+c):
-    return a+b+c 
->>>>>>> b75947cb620dadd50c48c05650b61251ff8fd324
 
 def subtract(a,b):
     return a-b
